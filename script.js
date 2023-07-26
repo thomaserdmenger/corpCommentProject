136a99e0df20eb62333e34997c80ad3da7e09f2c
Thomas Erdmenger <thomas.erdmenger@icloud.com>
2023-07-26 14:39:25 +0200

    Add input message to list

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-// -- COUNTER COMPONENT --
+// -- GLOBAL --
 const textAreaEl = document.querySelector(".form__textarea");
 const counterEl = document.querySelector(".counter");
+const feedbackListEl = document.querySelector(".feedbacks");
+const formEl = document.querySelector(".form");
 
+// -- COUNTER COMPONENT --
 const handleInput = () => {
   const limit = 150;
   const inputLength = textAreaEl.value.length;
@@ -9,3 +12,25 @@ const handleInput = () => {
 };
 
 textAreaEl.addEventListener("input", handleInput);
+
+// -- FORM COMPONENT --
+const handleSubmit = (event) => {
+  event.preventDefault();
+  let inputText = textAreaEl.value;
+  let listItem = `<li>${inputText}</li>`;
+
+  if (inputText.length > 5 && inputText.includes("#")) {
+    formEl.classList.add("form--valid");
+    setTimeout(() => formEl.classList.remove("form--valid"), 2000);
+  } else {
+    formEl.classList.add("form--invalid");
+    setTimeout(() => formEl.classList.remove("form--invalid"), 2000);
+    textAreaEl.focus();
+    return;
+  }
+
+  feedbackListEl.insertAdjacentHTML("beforeend", listItem);
+  textAreaEl.value = "";
+};
+
+formEl.addEventListener("submit", handleSubmit);
